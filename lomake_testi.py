4d0f105de5ec237cc7dd7a08dec8b6a7bb83c47f
Jussi Nurminen <jnu@iki.fi>
2016-01-29 09:40:48 +0200

    load ui directly

--- lomake_testi.py
+++ lomake_testi.py
@@ -9,28 +9,26 @@ https://nikolak.com/pyqt-qt-designer-getting-started/
 
 """
 
-from PyQt4 import QtGui
+from PyQt4 import QtGui, uic
 import sys
-import design
 
 
-class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
+class ExampleApp(QtGui.QMainWindow):
     def __init__(self):
-        # Explaining super is out of the scope of this article
-        # So please google it if you're not familar with it
-        # Simple reason why we use it here is that it allows us to
-        # access variables, methods etc in the design.py file
         super(self.__class__, self).__init__()
-        self.setupUi(self)  # This is defined in design.py file automatically
-                            # It sets up layout and widgets that are defined
+        uic.loadUi('tabbed_design.ui', self)
+        
 
 def main():
-    app = QtGui.QApplication(sys.argv)  # A new instance of QApplication
-    form = ExampleApp()                 # We set the form to be our ExampleApp (design)
-    form.show()                         # Show the form
-    app.exec_()                         # and execute the app
+    app = QtGui.QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    app.exec_()
 
-if __name__ == '__main__':              # if we're running file directly and not importing it
-    main()                              # run the main function
+if __name__ == '__main__':
+    main()
+    
+    
+
 
     
