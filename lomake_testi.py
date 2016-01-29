71de3a987b895f6cf3270375d388e0dc9da1451a
Jussi Nurminen <jnu@iki.fi>
2016-01-29 11:06:45 +0200

    report template into separate file

--- lomake_testi.py
+++ lomake_testi.py
@@ -1,27 +1,50 @@
 # -*- coding: utf-8 -*-
 """
-Qt designer / PyQt test
-first generate python code from design.ui by:
-pyuic4 design.ui >design.py
-
-see:
-https://nikolak.com/pyqt-qt-designer-getting-started/
 
 """
 
+from __future__ import print_function
+
+
 from PyQt4 import QtGui, uic
 import sys
+import report_templates
 
 
-class ExampleApp(QtGui.QMainWindow):
+class EntryApp(QtGui.QMainWindow):
+    """ Main window of application """
+    
     def __init__(self):
         super(self.__class__, self).__init__()
         uic.loadUi('tabbed_design.ui', self)
+        self.data = {}
+        # link buttons
+        self.btnReport.clicked.connect(self.make_report)
+        self.btnQuit.clicked.connect(self.quit)
+        
+    def closeEvent(self, event):
+        """ TODO: check whether user wants to exit, call event.reject() if not """
+        print("closing")
+        event.accept()
+            
+    def make_report(self):
+        """ Make textual report using the data from the entry fields """
+        self.gather()
+        report = report_templates.movement_report(self.data)
+        print(report.textual())
+        
+    def gather(self):
+        """ Gather all entered data into a dict. """
+        for ln in self.findChildren(QtGui.QLineEdit):
+            self.data[str(ln.objectName())] = str(ln.text())
+       
+    def quit(self):
+        pass
         
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    form = ExampleApp()
+    form = EntryApp()
     form.show()
     app.exec_()
 
---
+++ report_templates.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 10:37:47 2016
+
+@author: jussi
+"""
+
+class movement_report():
+    """ Make a liikelaajuus (movement range) report based on given data. """
+
+    def __init__(self, data):
+        self.data = data
+
+    def header(self):
+        """ Start of report, basic data (name etc.) """
+        return """Päivämäärä: {}
+Potilaskoodi: {}
+Potilaan nimi: {}
+Henkilötunnus: {}
+Diagnoosi: {}
+Mittaajat: {}""".format(self.data['lnDate'], self.data['lnID'], self.data['lnName'], 
+                        self.data['lnHetu'], self.data['lnDiag'], self.data['lnPersonnel'])
+           
+    def textual(self):
+        return self.header()
+    
+
+     
+     
+     
+ 
+
+
