048da1f6f9a76a434d665cd213cf4ecfe20fc0b3
Jussi Nurminen <jnu@iki.fi>
2016-02-02 13:58:08 +0200

    faster restore

--- lomake_testi.py
+++ lomake_testi.py
@@ -21,7 +21,7 @@ from PyQt4 import QtGui, uic
 import sys
 import report_templates
 import pickle
-
+import copy
 
 class EntryApp(QtGui.QMainWindow):
     """ Main window of application """
@@ -30,6 +30,9 @@ class EntryApp(QtGui.QMainWindow):
         super(self.__class__, self).__init__()
         uic.loadUi('tabbed_design.ui', self)
         self.data = {}
+        # save empty form (default states for widgets)
+        self.read_forms()
+        self.data_empty = copy.deepcopy(self.data)
         # these are magic values for entries not measured (default)
         self.LN_NONE = ''
         self.SP_NONE = -181
@@ -38,6 +41,7 @@ class EntryApp(QtGui.QMainWindow):
         # link buttons
         self.btnSave.clicked.connect(self.save)
         self.btnLoad.clicked.connect(self.load)
+        self.btnClear.clicked.connect(self.clear_forms)
         self.btnReport.clicked.connect(self.make_report)
         self.btnQuit.clicked.connect(self.quit)
         # TODO: set validators
@@ -65,46 +69,48 @@ class EntryApp(QtGui.QMainWindow):
         fh = open('save.p', 'rb')
         self.data = pickle.load(fh)
         self.restore_forms()
+        
+    def clear_forms(self):
+        """ Set form data to default. """
+        self.data = copy.deepcopy(self.data_default)
+        self.restore_forms()
     
     def restore_forms(self):
         """ Restore saved data into the input form. """
-        for wname in self.data:
-            val = self.data[wname]
-            widget = self.findChildren(QtGui.QWidget, wname)[0]
-            if wname[:2] == 'ln':
-                widget.setText(self.data[wname])
-            if wname[:2] == 'sp':
-                widget.setValue(self.data[wname])
-            if wname[:2] == 'cb':
-                widget.setCurrentIndex(widget.findData(self.data[wname]))
+        for ln in self.findChildren(QtGui.QLineEdit):
+            name = str(ln.objectName())
+            if name[:2] == 'ln':  # exclude spinboxes line edit objects
+                ln.setText(self.data[name])            
+        for sp in self.findChildren(QtGui.QSpinBox):
+            name = str(sp.objectName())
+            sp.setValue(self.data[name])
+        for cb in self.findChildren(QtGui.QComboBox):
+            name = str(cb.objectName())            
+            cb.setCurrentIndex(cb.findText(self.data[name]))        
+        for xb in self.findChildren(QtGui.QCheckBox):
+            name = xb.objectName()
+            xb.setCheckState(self.data[name])
+        for te in self.findChildren(QtGui.QTextEdit):
+            name = te.objectName()
+            te.setPlainText(self.data[name])
         
     def read_forms(self):
-        """ Gather all entered data into a dict, converting
+        """ Read all entered data into a dict, converting
         to Python types. Dict keys will be set according to 
         input widget names. """
         for ln in self.findChildren(QtGui.QLineEdit):
             name = str(ln.objectName())
             if name[:2] == 'ln':  # exclude spinboxes line edit objects
                 val = str(ln.text())
-                if val == self.LN_NONE:
-                    val = None
                 self.data[name] = val
         for sp in self.findChildren(QtGui.QSpinBox):
             val = int(sp.value())
-            if val == self.SP_NONE:
-                val = None
             self.data[str(sp.objectName())] = val
         for cb in self.findChildren(QtGui.QComboBox):
             val = str(cb.currentText())
-            if val == self.CB_NONE:
-                val = None
             self.data[str(cb.objectName())] = val
         for xb in self.findChildren(QtGui.QCheckBox):
             val = xb.checkState()
-            if val:
-                val = True
-            else:
-                val = False
             self.data[str(xb.objectName())] = val
         for te in self.findChildren(QtGui.QTextEdit):
             val = te.toPlainText()
