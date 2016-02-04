3ba4efcc23ea93ef96f569d5697cd014e1daa104
Jussi Nurminen <jnu@iki.fi>
2016-02-04 13:39:37 +0200

    html writer wip

--- report_templates.py
+++ report_templates.py
@@ -5,6 +5,53 @@ Created on Fri Jan 29 10:37:47 2016
 @author: jussi
 """
 
+from pandas import DataFrame
+
+    
+class html_report():
+
+    def __init__(self, data):
+        self.data = data
+        
+    def doc_header(self):
+        return """
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" /> 
+</head>
+<body>
+"""
+
+    def doc_terminate(self):
+        return """
+</body>
+</html>
+"""
+        
+    def sec_tiedot(self):
+        return """
+<h2>Potilaan tiedot</h2>
+<table style="width:100%">
+  <tr>
+    <td>Potilaskoodi</td>
+    <td>{lnTiedotID}</td> 
+  </tr>
+  <tr>
+    <td>Potilaan nimi</td>
+    <td>{lnTiedotNimi}</td> 
+  </tr>
+  <tr>
+    <td>Henkilötunnus</td>
+    <td>{lnTiedotHetu}</td> 
+  </tr>
+</table>
+""".format(**self.data)
+
+
+    def make(self):
+        return self.doc_header() + self.sec_tiedot() + self.doc_terminate()
+
 class movement_report():
     """ Make a report based on given data. """
 
@@ -21,20 +68,37 @@ class movement_report():
                 self.data[key] == None
             if key[:2] == 'cb' and data[key] == self.CB_NONE:
                 self.data[key] == None
-        self.data['separator'] = '---'
+        self.data['ITEM_SEP'] = '\t'
+        self.data['SECTION_SEP'] = ''
+        
+    def docx(self):
+        """ Generate a Word report """
+
+    def html(self):
+        rep = html_report(self.data)
+        return rep.make()
 
     def text(self):
         """ Generate a text report (multiline string) """
-        return u"""{separator}
-Päivämäärä: {lnTiedotPvm}
-Potilaskoodi: {lnTiedotID}
-Potilaan nimi: {lnTiedotNimi}
-Henkilötunnus: {lnTiedotHetu}
-Diagnoosi: {lnTiedotDiag}
-Mittaajat: {lnTiedotMittaajat}
+        return u"""{SECTION_SEP}
+Päivämäärä{ITEM_SEP}{lnTiedotPvm}
+Potilaskoodi{ITEM_SEP}{lnTiedotID}
+Potilaan nimi{ITEM_SEP}{lnTiedotNimi}
+Henkilötunnus{ITEM_SEP}{lnTiedotHetu}
+Diagnoosi{ITEM_SEP}{lnTiedotDiag}
+Mittaajat{ITEM_SEP}{lnTiedotMittaajat}
 Kommentit:\n{cmtTiedot}
-{separator}
+{SECTION_SEP}
 """.format(**self.data)
+
+    def excel(self, fn):
+        """ Export report to Excel (filename fn) """
+        # example (two columns:)
+        # df = DataFrame( {'Item': list_items, 'Value': list_values} )
+        # df.to_excel('test.xlsx', sheet_name='sheet1', index=False)
+        
+        
+        
            
     
 
