76f02045c771b490ed2266f83a594cd1cf55dfc7
Jussi Nurminen <jnu@iki.fi>
2016-02-08 15:25:45 +0200

    names into separate file

---
+++ ll_msgs.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 08 15:15:03 2016
+
+@author: hus20664877
+"""
+
+
+temp_found = u'Väliaikainen tallennustiedosto löytyi. Ohjelmaa ei ehkä suljettu oikein. Yritetään ladata tiedosto.'
+clear = u'Haluatko varmasti tyhjentää kaikki tiedot?'
+quit_ = u'Haluatko varmasti sulkea ohjelman?'
+open_title = u'Avaa tiedosto'
+save_title = u'Tallenna tiedosto'
+message_title = u'Liikelaajuus'
+yes_button = u'Kyllä'
+no_button = u'Ei'
+ok_button = u'Ok'
+
+
