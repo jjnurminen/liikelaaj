7b43de16a3cfa021ce32e4bc0f96d81d522d0c69
Jussi Nurminen <jnu@iki.fi>
2016-03-02 10:14:13 +0200

    testing

--- pandas_test.py
+++ pandas_test.py
@@ -7,20 +7,49 @@ Created on Thu Feb 04 10:44:07 2016
 
 from __future__ import print_function
 import string
+import sys
 
 di = {'a': 1, 'b': 2, 'c': 3, 'x': 4, 'y': None}
-text = {}
 
-tli = ['JOTAIN',
-'a: {a} b: {b}', 'x: {x}', 'b: {b}', 'c: {c}', 'y: {y}']
+tli = ['JOTAIN', 'a: {a} b: {b}', 'x: {x}', 'b: {b}', 'c: {c}', 'y: {y}']
+
+s = tli[1]
 
 
-fi = string.Formatter()
+def get_field(s):
+    fi = string.Formatter()
+    pit = fi.parse(s)  # parser iterator
+    for items in pit:
+        yield items[1]
+    
+def format_item(s, di):
+    flds = list(get_field(s))
+    if not any(flds):
+        return s
+    else:
+        if any([di[fld] for fld in flds]):
+            return s.format(**di)            
+
+def noval():
+    pass
+
+for fld in get_field(s):
+    print(fld)
+    
+sys.exit()
+    
+for s,i in enumerate(tli):
+    print(i)
+    for fi in get_field(s):
+        print(fi)
+
+sys.exit()
+
 
 
 
 """ Conditional formatter for a list of string items:
-Format only items that have their fields defined in di.
+Format only items that have their fields defined in di. (TODO: some fields)
 Assumes one field per item (only looks at the first field).
 Items whose fields are not present are excluded. 
 Returns a new list. """
@@ -41,6 +70,9 @@ print(cond_format(tli, di))
     
 
 
+
+
+
 """ Conditional formatter for iterable of strings:
 Format only strings that have their fields defined in di.
 Assumes one field per item (only looks at the first field).
@@ -50,12 +82,13 @@ def cond_format(items, di):
     fi = string.Formatter()
     for st in items:
         pai = fi.parse(st)  # parsing iterator that returns tuples of strings, fields etc.
-        fld = pai.next()[1]
-        if fld:
-            if di[fld]:
-                yield st.format(**di)
-        else:
-            yield st
+        for fld in pai:
+            fld = pai.next()[1]
+            if fld:
+                if di[fld]:
+                    yield st.format(**di)
+            else:
+                yield st
 
 cfo = cond_format(tli, di)    
 
