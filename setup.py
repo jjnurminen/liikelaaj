cc706551853902a2389841277156ff5061d28dc0
Jussi Nurminen <jnu@iki.fi>
2019-03-15 15:43:20 +0200

    wip: code into subdir

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jussi (jnu@iki.fi)
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(name='liikelaaj',
+      version='0.10',
+      description='Input and report movement range data',
+      author='Jussi Nurminen',
+      author_email='jnu@iki.fi',
+      license='GPLv3',
+      url='https://github.com/jjnurminen/liikelaaj',
+      packages=find_packages(),
+      entry_points={
+              'console_scripts': ['liikelaaj=liikelaaj:main']
+              },
+      include_package_data=True,
+      )
